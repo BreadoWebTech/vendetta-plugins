5c9aa8addbe583aa8d1fa9708b7876300eb53917
Acquite <acquitefx@gmail.com>
2023-01-16 22:09:56 +0000

    remove logger

--- plugins/template/src/index.js
+++ plugins/template/src/index.js
@@ -1,5 +1,3 @@
-const { logger } = vendetta;
-
 export default {
     onLoad: async function() {
         const res = fetch("https://raw.githubusercontent.com/acquitelol/dislate/main/dist/Dislate.js")
